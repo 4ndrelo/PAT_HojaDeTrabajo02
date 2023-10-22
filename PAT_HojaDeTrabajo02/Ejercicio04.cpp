aab316c39e2179a545a31a04200ddf834733213c
4ndrelo <allanda23456@gmail.com>
2023-10-22 14:37:44 -0600

    Ejercicio 3 y 4

--- PAT_HojaDeTrabajo02/Ejercicio04.cpp
+++ PAT_HojaDeTrabajo02/Ejercicio04.cpp
@@ -2,31 +2,118 @@
 
 FrontMiddleBackQueue::FrontMiddleBackQueue()
 {
+    head = nullptr;
+    size = 0;
 }
 
 void FrontMiddleBackQueue::pushFront(int value)
 {
+    Node<int>* newNode = new Node<int>{ value, head };
+    head = newNode;
+    size++;
 }
 
 void FrontMiddleBackQueue::pushMiddle(int value)
 {
+    if (size == 0)
+    {
+        pushFront(value);
+        return;
+    }
+
+    int middlePos = (size - 1) / 2;
+    Node<int>* current = head;
+    for (int i = 0; i < middlePos; i++)
+    {
+        current = current->next;
+    }
+
+    Node<int>* newNode = new Node<int>{ value, current->next };
+    current->next = newNode;
+    size++;
 }
 
 void FrontMiddleBackQueue::pushBack(int value)
 {
+    if (size == 0)
+    {
+        pushFront(value);
+        return;
+    }
+
+    Node<int>* current = head;
+    while (current->next)
+    {
+        current = current->next;
+    }
+
+    Node<int>* newNode = new Node<int>{ value, nullptr };
+    current->next = newNode;
+    size++;
 }
 
 int FrontMiddleBackQueue::popFront()
 {
-	return 0;
+    if (size == 0)
+    {
+        return -1;
+    }
+
+    int value = head->value;
+    Node<int>* temp = head;
+    head = head->next;
+    delete temp;
+    size--;
+    return value;
 }
 
 int FrontMiddleBackQueue::popMiddle()
 {
-	return 0;
+    if (size == 0)
+    {
+        return -1;
+    }
+
+    int middlePos = (size - 1) / 2;
+    Node<int>* current = head;
+    for (int i = 0; i < middlePos; i++)
+    {
+        current = current->next;
+    }
+
+    int value = current->next->value;
+    Node<int>* temp = current->next;
+    current->next = temp->next;
+    delete temp;
+    size--;
+    return value;
 }
 
 int FrontMiddleBackQueue::popBack()
 {
-	return 0;
+    if (size == 0)
+    {
+        return -1;
+    }
+
+    if (size == 1)
+    {
+        int value = head->value;
+        delete head;
+        head = nullptr;
+        size = 0;
+        return value;
+    }
+
+    Node<int>* current = head;
+    while (current->next->next)
+    {
+        current = current->next;
+    }
+
+    int value = current->next->value;
+    delete current->next;
+    current->next = nullptr;
+    size--;
+    return value;
 }
