21e87ba359a6162d6f4d60d9fe7d5b6559d93352
4ndrelo <allanda23456@gmail.com>
2023-10-22 14:18:38 -0600

    Ejercicio 1 y 2

--- PAT_HojaDeTrabajo02/Ejercicio01.cpp
+++ PAT_HojaDeTrabajo02/Ejercicio01.cpp
@@ -2,5 +2,33 @@
 
 Node<char>* Ejercicio01::rotateRight(Node<char>* head, int k)
 {
-    return nullptr;
+    if (!head || k == 0) {
+        return head;
+    }
+
+    int length = 0;
+    Node<char>* current = head;
+    while (current->next) {
+        length++;
+        current = current->next;
+    }
+    length++;
+
+
+    k = k % length;
+    if (k == 0) {
+        return head;
+    }
+
+    int newHeadPos = length - k;
+    current->next = head;
+    current = head;
+    for (int i = 0; i < newHeadPos - 1; i++) {
+        current = current->next;
+    }
+
+
+    Node<char>* newHead = current->next;
+    current->next = nullptr;
+    return newHead;
 }
--- PAT_HojaDeTrabajo02/Ejercicio02.cpp
+++ PAT_HojaDeTrabajo02/Ejercicio02.cpp
@@ -2,5 +2,26 @@
 
 Node<int>* Ejercicio02::addTwoNumbers(Node<int>* l1, Node<int>* l2)
 {
-    return nullptr;
+    Node<int>* dummy = new Node<int>();
+    Node<int>* current = dummy;
+    int carry = 0;
+
+    while (l1 || l2 || carry) {
+        int x = (l1) ? l1->value : 0;
+        int y = (l2) ? l2->value : 0;
+
+        int sum = x + y + carry;
+        carry = sum / 10;
+        int digit = sum % 10;
+
+        current->next = new Node<int>();
+        current = current->next;
+        current->value = digit;
+
+        if (l1) l1 = l1->next;
+        if (l2) l2 = l2->next;
+    }
+
+
+    return dummy->next;
 }
